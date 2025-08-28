43b4fe4c84f90c1c2f4f7a6f8fca413a8c36e1cc
helloTchek <hello@tchek.ai>
2025-08-28 15:52:34 +0200

    Updated CompaniesPage.tsx

--- src/pages/Companies/CompaniesPage.tsx
+++ src/pages/Companies/CompaniesPage.tsx
@@ -172,7 +172,7 @@ export default function CompaniesPage() {
         <div className="space-y-4">
           <p className="text-gray-600">
             Are you sure you want to duplicate <strong>{duplicateModal.company?.name}</strong>? 
-            A new company will be created with "(Copy)" appended to the name.
+            A new company will be created with "(Copy)\" appended to the name.
           </p>
           <div className="bg-blue-50 border border-blue-200 rounded-lg p-3">
             <h4 className="text-sm font-medium text-blue-900 mb-2">What will be duplicated:</h4>
